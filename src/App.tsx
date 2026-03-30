1f506abc70a65d4d8caa5e5b32290022d185d384
Akshay <akshay.chilad@webengage.com>
2026-03-30 09:54:15 +0530

    WebEngage Integration successfully done - Android

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { Route, Redirect } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import { Webengage } from '@awesome-cordova-plugins/webengage';
+
 
 setupIonicReact();
 
-const App: React.FC = () => (
+const App: React.FC = () => {
+  useEffect(() => {
+    Webengage.engage();
+  }, []);
+  return (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
@@ -17,6 +23,7 @@ const App: React.FC = () => (
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
-);
+  );
+};
 
 export default App;
